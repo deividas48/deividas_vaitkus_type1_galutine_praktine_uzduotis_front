e05a1ac42557667c58b08be472a6f9eae85fb350
deividas48 <deividas.v6@gmail.com>
2024-04-11 13:15:39 +0300

    Create a file that separates hero

---
+++ src/components/Hero.jsx
@@ -0,0 +1,15 @@
+export default function Hero() {
+  return (
+    <section className="md:py-12 text-center hidden sm:block">
+      <h2 className="leading-6 text-custom-primary-color uppercase font-medium">
+        Our Latest Listing
+      </h2>
+      <p className="mt-2 text-3xl leading-8 font-semibold tracking-tight md:text-4xl">
+        New Listings in Our Directory
+      </p>
+      <p className="mt-3 text-center text-custom-gray-color">
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+      </p>
+    </section>
+  );
+}
