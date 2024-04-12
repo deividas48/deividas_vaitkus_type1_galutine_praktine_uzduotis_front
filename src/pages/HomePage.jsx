d341933998509fedb3735208c7f5cf3a72c6ba6d
deividas48 <deividas.v6@gmail.com>
2024-04-12 14:27:41 +0300

    Just side and aside coloring

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -33,34 +33,17 @@ function HomePage() {
 
   return (
     <div>
-
+<Hero />
       <div className="flex min-h-screen">
-        <aside className="w-1/4 bg-green-100">
-          <div className=" bg-red-100 p-4 pb-1 mb-4 rounded-lg"><CategoryPage /></div>
-          <div className=" bg-red-100 p-4 pb-1 rounded-lg"><CategoryPage /></div>
+        <aside className="w-1/4">
+          <div className=" bg-white p-4 pb-1 mb-4 rounded-lg"><CategoryPage /></div>
+          <div className=" bg-white p-4 pb-1 rounded-lg"><CategoryPage /></div>
         </aside>
-        <main className="w-3/4 bg-blue-100 p-4">
+        <main className="w-3/4 p-4 pt-0">
           {/* <h1>Our listings</h1> */}
           <AdsPage />
-          {/* {isLoading && <p>Loading...</p>}
-          {isError && <p>Failed to fetch ads</p>}
-          {!isLoading && !isError && <AdsList list={adsArr} />} */}
         </main>
       </div>
-
-      <div>
-        <AdsPage />
-      </div>
-      <div>
-        <CategoryPage />
-      </div>
-      <div>
-        <CategoryPage />
-      </div>
-      <Hero />
-      {/* {isLoading && <p>Loading...</p>}
-      {isError && <p>Failed to fetch ads</p>}
-      {!isLoading && !isError && <AdsList list={adsArr} />} */}
     </div>
   );
 }
