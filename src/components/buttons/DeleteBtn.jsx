d859f16644f6382997580c89747c68ecb4846476
deividas48 <deividas.v6@gmail.com>
2024-04-15 05:22:54 +0300

    repair delete button

--- src/components/buttons/DeleteBtn.jsx
+++ src/components/buttons/DeleteBtn.jsx
@@ -5,7 +5,7 @@ export default function DeleteBtn() {
   return (
     <NavLink
       to="/skelbimas/sukurti"
-      className="text-white rounded-full hover-bg-gradient relative z-10 font-black flex items-center justify-center w-3/5 md:w-4/5"
+      className="text-white rounded-full hover-bg-gradient relative z-10 font-black flex items-center justify-center p-2"
     >
       <span className="ml-3 header-įdėti-button-pavadinimas">Delete ad</span>
     </NavLink>
