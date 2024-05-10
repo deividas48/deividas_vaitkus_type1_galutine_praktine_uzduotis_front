ba2f677929b9acc0831ae516c7ec55370263d61a
deividas48 <deividas.v6@gmail.com>
2024-05-10 16:20:58 +0300

    Forms. Recreate form and create submit button

---
+++ src/components/buttons/Submit.jsx
@@ -0,0 +1,5 @@
+export default function Submit() {
+  return (
+    <div className="bg-custom-primary-color text-white rounded-md py-2 px-4 transition-all duration-300 ease-in-out hover:bg-custom-color-secondary">Add</div>
+  );
+}
--- src/pages/AddAdPage.jsx
+++ src/pages/AddAdPage.jsx
@@ -1,8 +1,11 @@
 import { useFormik } from 'formik';
+import '../styles/Forms.css';
+import Submit from '../components/buttons/Submit';
 
 export default function AddAdPage() {
   const formik = useFormik({
     initialValues: {
+      // Create initial values for the form
       title: '',
       description: '',
       price: '',
@@ -17,96 +20,44 @@ export default function AddAdPage() {
   });
 
   return (
-    <div className="flex flex-col items-center min-h-screen bg-white mt-4 mb-4 p-4 rounded-lg">
+    <div className="items-center min-h-screen bg-white mt-4 mb-4 p-4 rounded-lg">
       <form
         onSubmit={formik.handleSubmit}
-        className="flex flex-col rounded-lg p-4 shadow-md custom_form items-center"
+        className="flex flex-col py-4 w-full"
       >
-        <div className="flex w-full">
-          <label htmlFor="title" className="w-1/2 text-right mr-1">Title:</label>
-          <input
-            id="title"
-            name="title"
-            type="text"
-            onChange={formik.handleChange}
-            value={formik.values.title}
-            placeholder="Title"
-            className="w-1/2"
-          />
+        {[
+          'title',
+          'description',
+          'price',
+          'phone',
+          'type',
+          'town',
+          'category',
+        ].map((field) => (
+          // key is required for React to keep track of the elements
+          <div key={field} className="custom_form_pairs">
+            <label htmlFor={field}>
+              {/* Make label name from already created variables. */}
+              {field.charAt(0).toUpperCase() + field.slice(1)}
+              :
+            </label>
+            <input
+              id={field}
+              name={field}
+              type="text"
+              onChange={formik.handleChange}
+              value={formik.values[field]}
+              // placeholder={field.charAt(0).toUpperCase() + field.slice(1)}
+            />
+          </div>
+        ))}
+        <div className="flex justify-center">
+          <div className="flex w-4/5 md:w-2/4 justify-end">
+            <button type="submit" aria-label="Submit form">
+              <Submit />
+            </button>
+          </div>
         </div>
-        <div className="flex w-full">
-          <label htmlFor="description" className="w-1/2 text-right mr-1">Description:</label>
-          <input
-            id="description"
-            name="description"
-            type="text"
-            onChange={formik.handleChange}
-            value={formik.values.description}
-            placeholder="Description"
-            className="w-1/2"
-          />
-        </div>
-        <div className="flex w-full">
-          <label htmlFor="price" className="w-1/2 text-right mr-1">Price:</label>
-          <input
-            id="price"
-            name="price"
-            type="text"
-            onChange={formik.handleChange}
-            value={formik.values.price}
-            placeholder="Price"
-            className="w-1/2"
-          />
-        </div>
-        <div className="flex w-full">
-          <label htmlFor="phone" className="w-1/2 text-right mr-1">Phone:</label>
-          <input
-            id="phone"
-            name="phone"
-            type="text"
-            onChange={formik.handleChange}
-            value={formik.values.phone}
-            placeholder="Phone"
-            className="w-1/2"
-          />
-        </div>
-        <div className="flex w-full">
-          <label htmlFor="type" className="w-1/2 text-right mr-1">Type:</label>
-          <input
-            id="type"
-            name="type"
-            type="text"
-            onChange={formik.handleChange}
-            value={formik.values.type}
-            placeholder="Type"
-            className="w-1/2"
-          />
-        </div>
-        <div className="flex w-full">
-          <label htmlFor="town" className="w-1/2 text-right mr-1">Town:</label>
-          <input
-            id="town"
-            name="town"
-            type="text"
-            onChange={formik.handleChange}
-            value={formik.values.town}
-            placeholder="Town"
-            className="w-1/2"
-          />
-        </div>
-        <div className="flex w-full">
-          <label htmlFor="category" className="w-1/2 text-right mr-1">Category:</label>
-          <input
-            id="category"
-            name="category"
-            type="text"
-            onChange={formik.handleChange}
-            value={formik.values.category}
-            placeholder="Category"
-            className="w-1/2"
-          />
-        </div>
-        <button type="submit">Submit</button>
       </form>
     </div>
   );
