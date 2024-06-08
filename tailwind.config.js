a82d2a337d05e597f9c91c399278de041a79552f
deividas48 <deividas.v6@gmail.com>
2024-06-08 19:49:19 +0300

    Ensure the container is full width and expands consistently

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,13 +6,19 @@ export default {
       container: {
         center: true,
         padding: '1rem',
+        screens: {
+          sm: '100%', // mobilus iki
+          md: '100%', // plansetes iki
+          lg: '100%', // desktopas iki
+          xl: '100%', // super platus iki
+        }, // Ensure the container is full width and expands consistently
       },
       screens: {
-        sm: '350px', // mobilus iki
-        md: '768px', // plansetes iki (senas 768px)
-        lg: '1024px', // desktopas iki (senas 1024px)
-        xl: '1500px', // super platus iki
-      },
+        sm: '350px', // Mobile up to
+        md: '768px', // Tablet up to
+        lg: '1024px', // Desktop up to
+        xl: '1500px', // Super wide up to
+      }, // Set up screen sizes for responsive design
       colors: {
         'custom-primary-color': '#ff3c48',
         'custom-gray-color': '#646464',
