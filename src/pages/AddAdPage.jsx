18e22827f06630ec8d029074213c96ce2daf2eef
deividas48 <deividas.v6@gmail.com>
2024-06-10 18:12:28 +0300

    Forms. Create a mobile view for the line consisting of two inputs

--- src/pages/AddAdPage.jsx
+++ src/pages/AddAdPage.jsx
@@ -95,7 +95,7 @@ export default function AddAdPage() {
             // width 10 proc.
             className="pairs_input_firstHalf_width_12_5proc pairs_input_notFull"
           />
-          <label htmlFor="phone" className="pairs_label_secoundHalf pairs_label_secHalf_width_12_5proc ml-3">
+          <label htmlFor="phone" className="pairs_label_secoundHalf pairs_label_secHalf_width_12_5proc ml-3 pairs_label_full_md">
             Phone:
           </label>
           <input
