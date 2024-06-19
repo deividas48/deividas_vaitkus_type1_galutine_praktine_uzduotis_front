64d9f1d677ff3183236a50e1ec126fd876a9d540
deividas48 <deividas.v6@gmail.com>
2024-06-19 22:25:46 +0300

    List. Just comment the ads list script

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -11,15 +11,23 @@ function HomePage() {
   return (
     <div>
       <Hero2 />
+      {/* All the ads and categories are displayed here */}
       <div className="md:flex min-h-screen">
         <aside className="md:w-1/4">
           <div>
-            <div className=" bg-white p-4 pt-1 mb-4 rounded-lg"><TownsPage /></div>
+            {/* All the towns are displayed here */}
+            <div className=" bg-white p-4 pt-1 mb-4 rounded-lg">
+              <TownsPage />
+            </div>
           </div>
-          <div className=" bg-white p-4 pb-1 rounded-lg"><CategoryPage /></div>
+          {/* All the categories are displayed here */}
+          <div className=" bg-white p-4 pb-1 rounded-lg">
+            <CategoryPage />
+          </div>
         </aside>
         <main className="md:w-3/4 my-4 md:my-0 md:p-4 md:pt-0">
           {/* <h1>Our listings</h1> */}
+          {/* All the ads are displayed here */}
           <AdsPage />
         </main>
       </div>
