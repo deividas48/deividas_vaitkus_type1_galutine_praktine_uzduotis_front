87f5418c2e70cc89e95f25eba6fa29142bb8b0a0
deividas48 <deividas.v6@gmail.com>
2024-07-04 13:17:24 +0300

    refactor: improve comments for clarity and consistency

--- src/pages/PageHome.jsx
+++ src/pages/PageHome.jsx
@@ -10,17 +10,18 @@ import ListingsPage from '../components/listings/ListingsPage';
 
 function PageHome() {
   // #Create_sort - create short function for sorting listings.
-  // #1_create_sort. Create the state to store the sort option.
+  // 1_#create_sort. Create the state to store the sort option.
   // 'price-asc' - sort by price ascending.
   const [sortOption, setSortOption] = useState('price-asc');
 
   return (
     <div>
       <Hero2 />
-      {/* #2_create_sort. Create the div for the sort dropdown. */}
+      {/*  */}
       <div className="mb-4 flex mr-4">
         {/* Just to make blank space */}
         <span className="md:w-3/5"> </span>
+        {/* 2_#create_sort - create the div for the sort dropdown. */}
         <div className="md:w-2/5 flex">
           <label
             htmlFor="sort"
@@ -31,14 +32,14 @@ function PageHome() {
           <select
             id="sort"
             className="mt-1 block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md"
-              // The value of the dropdown is the sortOption state.
+            // The value of the dropdown is the sortOption state.
             value={sortOption}
-              // #2.1_create_sort. Update the SortOption state. OnChange event - when the value
-              // of the dropdown changes, the setSortOption function is called with the new
-              // value.
+            // 2.1_#create_sort. Update the SortOption state. OnChange event - when the value
+            // of the dropdown changes, the setSortOption function is called with the new
+            // value.
             onChange={(e) => setSortOption(e.target.value)}
           >
-            {/* #2.2_create_sort. Add the options for sorting. */}
+            {/* 2.2_#create_sort. Add the options for sorting. */}
             <option value="price-asc">Price: Low to High</option>
             <option value="price-desc">Price: High to Low</option>
           </select>
@@ -55,7 +56,7 @@ function PageHome() {
         <main className="md:w-3/4 my-4 md:my-0 md:p-4 md:pt-0">
           {/* <h1>Our listings</h1> */}
           {/* All the listings are displayed here */}
-          {/* #3_create_sort. Pass the sortOption state to the ListingsPage file. */}
+          {/* 3_#create_sort. Pass the sortOption state to the ListingsPage file. */}
           <ListingsPage sortOption={sortOption} />
         </main>
       </div>
