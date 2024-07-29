37589b95674b62de9781f5642ef787512e0465ff
deividas48 <deividas.v6@gmail.com>
2024-07-29 20:20:30 +0300

    feat: Add price formatting to filter inputs
    - Implemented handlePriceBlur function to format price inputs to two decimal places on blur event.
    - Integrated handlePriceBlur into Formik Field components for minPrice and maxPrice.
    - Ensured price values are parsed and formatted correctly.
    - Updated FiltersLayoutBasePages component to include price formatting functionality.

--- src/components/filters/FiltersLayoutBasePages.jsx
+++ src/components/filters/FiltersLayoutBasePages.jsx
@@ -1,18 +1,20 @@
 // FiltersLayoutBasePages.jsx
 // #CreateFiltersLayoutBasePages
 
-import { Formik, Form, Field, ErrorMessage } from 'formik';
+import {
+  Formik, Form, Field, ErrorMessage,
+} from 'formik';
 import * as Yup from 'yup';
 import '../../styles/Forms.css';
 
 const FilterSchema = Yup.object().shape({
   minPrice: Yup.number().min(
     0,
-    'Minimum price must be greater than or equal to 0'
+    'Minimum price must be greater than or equal to 0',
   ),
   maxPrice: Yup.number().min(
     0,
-    'Maximum price must be greater than or equal to 0'
+    'Maximum price must be greater than or equal to 0',
   ),
   town: Yup.string(),
   type: Yup.string(),
@@ -20,6 +22,17 @@ const FilterSchema = Yup.object().shape({
 });
 
 function FiltersLayoutBasePages({ onFilterChange }) {
+  // Function to handle the price input onBlur event
+  const handlePriceBlur = (event, setFieldValue) => {
+    const { name, value } = event.target;
+    // Parse the input value to a float
+    const floatValue = parseFloat(value);
+    if (!Number.isNaN(floatValue)) {
+      // Set the field value to a fixed two decimal places
+      setFieldValue(name, floatValue.toFixed(2));
+    }
+  };
+
   return (
     <Formik
       initialValues={{
@@ -36,33 +49,65 @@ function FiltersLayoutBasePages({ onFilterChange }) {
         setSubmitting(false);
       }}
     >
-      {({ isSubmitting }) => (
+      {({
+        isSubmitting, setFieldValue, handleChange, handleBlur,
+      }) => (
         <Form className="">
           <div className="filter-field">
             <label htmlFor="minPrice" className="pairs_label_full">
               Min Price
             </label>
-            <Field type="number" name="minPrice" className="pairs_input_full w-16" />
+            <Field
+              type="number"
+              name="minPrice"
+              className="pairs_input_full w-16"
+              onChange={handleChange}
+              onBlur={(e) => {
+                handlePriceBlur(e, setFieldValue);
+                handleBlur(e);
+              }}
+            />
             <ErrorMessage name="minPrice" component="div" />
           </div>
           <div className="filter-field">
             <label htmlFor="maxPrice">Max Price</label>
-            <Field type="number" name="maxPrice" className="pairs_input_full w-full w-16" />
+            <Field
+              type="number"
+              name="maxPrice"
+              className="pairs_input_full w-16"
+              onChange={handleChange}
+              onBlur={(e) => {
+                handlePriceBlur(e, setFieldValue);
+                handleBlur(e);
+              }}
+            />
             <ErrorMessage name="maxPrice" component="div" />
           </div>
           <div className="filter-field">
             <label htmlFor="town">Town</label>
-            <Field type="text" name="town" className="pairs_input_full w-full" />
+            <Field
+              type="text"
+              name="town"
+              className="pairs_input_full w-full"
+            />
             <ErrorMessage name="town" component="div" />
           </div>
           <div className="filter-field">
             <label htmlFor="type">Type</label>
-            <Field type="text" name="type" className="pairs_input_full w-full" />
+            <Field
+              type="text"
+              name="type"
+              className="pairs_input_full w-full"
+            />
             <ErrorMessage name="type" component="div" />
           </div>
           <div className="filter-field">
             <label htmlFor="seller">Seller</label>
-            <Field type="text" name="seller" className="pairs_input_full w-full" />
+            <Field
+              type="text"
+              name="seller"
+              className="pairs_input_full w-full"
+            />
             <ErrorMessage name="seller" component="div" />
           </div>
           <button type="submit" disabled={isSubmitting}>
