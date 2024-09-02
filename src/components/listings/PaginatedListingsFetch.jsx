34c4c3d12505a284e4043e9db970c9abf66d13e2
deividas48 <deividas.v6@gmail.com>
2024-09-02 22:32:45 +0300

    feat: submit button changes

--- src/components/listings/PaginatedListingsFetch.jsx
+++ src/components/listings/PaginatedListingsFetch.jsx
@@ -1,11 +1,28 @@
 // src/components/listings/PaginatedListingsFetch.jsx
 
-import React, { useEffect, useState } from 'react';
-import { useSearchParams } from 'react-router-dom'; // *Search in this case is just only URL maker. #URLmaker
+import React, { useEffect, useState, useRef } from 'react';
+import { useSearchParams, useLocation, useNavigate } from 'react-router-dom'; // #URLmaker
 import axios from 'axios';
 import ListingsList from './ListingsList';
 // import { useNavigate } from 'react-router-dom';
 import { useSearch } from '../context/SearchContext'; // Import the search context
+import { HOME_PAGE_PATH } from '../../config/routes';
+
+// Define the hook to clear the search term
+// Initialize searchTerm from the URL if it's present
+function useInitializeSearchTerm() {
+  const [searchParams] = useSearchParams();
+  const { searchTerm, setSearchTerm } = useSearch();
+  const initialSearchTerm = useRef(searchTerm);
+
+  useEffect(() => {
+    const newSearchTerm = searchParams.get('search');
+    if (newSearchTerm && initialSearchTerm.current !== newSearchTerm) {
+      setSearchTerm(newSearchTerm);
+      initialSearchTerm.current = newSearchTerm;
+    }
+  }, [searchParams, setSearchTerm]);
+}
 
 function PaginatedListingsFetch({
   currentPage, // Passed from LayoutBasePages.jsx
@@ -15,65 +32,80 @@ function PaginatedListingsFetch({
   setTotalPages, // Passed from LayoutBasePages.jsx to control pagination
   setCategoryName, // Passed from LayoutBasePages.jsx
 }) {
-  // // Log props received by PaginatedListingsFetch
-  // console.log('Props in PaginatedListingsFetch:', {
-  //   currentPage,
-  //   baseFilters,
-  //   sortOption,
-  //   categoryId,
-  //   setTotalPages,
-  // });
-
   const [listing, setListing] = useState([]);
   const [loading, setLoading] = useState(false); // For loading state
 
-  const { searchTerm } = useSearch(); // Access the search term from the context
+  const { searchTerm, setSearchTerm } = useSearch(); // Access the search term from the context
   // eslint-disable-next-line no-unused-vars
   const [searchParams, setSearchParams] = useSearchParams(); //  #URLmaker. When you
+  const previousParamsRef = useRef(); // to store previous parameters
   // call setSearchParams(params), it updates the URL so to use searchParams is't necessary
-  // const navigate = useNavigate(); // Used for programmatic navigation
+
+  // Initialize searchTerm when the component mounts
+  useInitializeSearchTerm();
+
+  const navigate = useNavigate(); // Used for programmatic navigation
+  const location = useLocation();
+
+  useEffect(() => {
+    // To quit base search query when home page is clicked
+    if (location.pathname === HOME_PAGE_PATH && !searchParams.has('search')) {
+      // console.log('location.pathname===', location.pathname);
+      // console.log('HOME_PAGE_PATH===', location.pathname);
+      // console.log(
+      // 'location.pathname === HOME_PAGE_PATH===',
+      // location.pathname === HOME_PAGE_PATH
+      // );
+      setSearchTerm(''); // Reset search term when navigating to the home page
+    }
+  }, [location, searchParams, setSearchTerm]);
 
   // For a main fetch.
   useEffect(() => {
-    setLoading(true);
-
-    // Create an object to hold query parameters
-    const params = {
-      page: currentPage,
-      sort: sortOption,
-      ...(categoryId && { category: categoryId }),
-      ...baseFilters,
-      // headerSearch: 'headerSearch',
-      ...(searchTerm && { search: searchTerm }), // Send searchTerm (search text) to the backend.
-    };
-
-    // Set URL params
-    setSearchParams(params); // The call will convert the object to a query string and update the
-    // URL like this: http://your-site-url.com/listings?page=2&sort=price-asc&category=3&minPrice=1000&maxPrice=5000&town=New%20York&type=sell. #URLmaker
-
-    // Construct query string for the API request
-    const apiUrl = `/api/listings?${new URLSearchParams(params).toString()}`; // #URLmaker
-    // console.log('Final API URL:', apiUrl); // Log the final URL
-
-    // Fetch listings whenever any dependency changes
     const fetchListings = async () => {
+      setLoading(true);
+
+      // Create an object to hold query parameters
+      const params = {
+        page: currentPage,
+        sort: sortOption,
+        ...(categoryId && { category: categoryId }),
+        ...baseFilters,
+        ...(searchTerm && { search: searchTerm }), // Send searchTerm (search text) to the backend.
+      };
+
+      // Convert the params object to a URLSearchParams instance
+      const newSearchParams = new URLSearchParams(params);
+      // console.log('newSearchParams', newSearchParams);
+
+      // Only replace the URL if the search parameters have not changed
+      if (newSearchParams.toString() !== previousParamsRef.current) {
+        setSearchParams(params); // Update the URL with new query params
+        // Store the current URL parameters in the previousParamsRef
+        previousParamsRef.current = newSearchParams.toString();
+      }
+
+      // if (newSearchParams.toString() !== searchParams.toString()) {
+      //   console.log('newSearchParams.toString()===', newSearchParams.toString());
+      //   console.log('searchParams.toString()===', searchParams.toString());
+      //   navigate(`?${newSearchParams.toString()}`, { replace: false });
+      // } else {
+      //   navigate(`?${newSearchParams.toString()}`, { replace: true });
+      // }
+
       try {
         // Make the API request with the constructed URL
-        const response = await fetch(apiUrl);
+        const response = await fetch(
+          `/api/listings?${newSearchParams.toString()}`
+        );
         if (!response.ok) {
           throw new Error('API Request failed');
         }
         const data = await response.json();
 
-        console.log('API Response Data:', data); // Log the fetched data
-        // console.log('API Response Data2:', data.listings); // Log the fetched data
-
         // Update state with the fetched listings and total pages
         setListing(data.listings || []);
-        // console.log('Final listings:', listing);
         setTotalPages(data.totalPages || 1); // Ensure totalPages is being set
-        // setCategoryName(data.listings[0].category_name);
-        // console.log('category_name', data.listings[0].category_name);
       } catch (error) {
         // eslint-disable-next-line no-console
         console.error('Error fetching listings:', error); // Log any errors
@@ -90,10 +122,8 @@ function PaginatedListingsFetch({
     categoryId,
     searchTerm, // Add searchTerm as a dependency to trigger search
     setTotalPages,
-    setSearchParams, // #URLmaker
+    // setSearchParams, // #URLmaker
   ]); // Dependencies
-
-  // console.log('Final listings2:', listing);
 
   // Add useEffect to fetch and set category name based on categoryId - just a side fetch.
   useEffect(() => {
