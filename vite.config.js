d5a81830484162c62bfcb57cdb0a43068fb8cdb0
deividas48 <deividas.v6@gmail.com>
2024-10-16 02:29:17 +0300

    feat: add dotenv file

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,6 @@
+// vite.config.js
+// *Vite will automatically use .env.production when building for production
+
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
